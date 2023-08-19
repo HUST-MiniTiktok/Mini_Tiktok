1f08fabbd8a3f2946ef749cb1329a45e90376955
alteracd <2997581916@qq.com>
2023-08-19 23:51:59 +0800

    improvement: redis hash

--- pkg/mw/redis/ops.go
+++ pkg/mw/redis/ops.go
@@ -3,10 +3,11 @@ package redis
 import (
 	"strconv"
 	"time"
+
 	"github.com/cloudwego/kitex/pkg/klog"
 )
 
-func (c *RDClient) Get(key string) (string) {
+func (c *RDClient) Get(key string) string {
 	cmd := c.Client.Get(key)
 	val, err := cmd.Result()
 	if err != nil {
@@ -15,7 +16,7 @@ func (c *RDClient) Get(key string) (string) {
 	return val
 }
 
-func (c *RDClient) GetInt(key string) (int) {
+func (c *RDClient) GetInt(key string) int {
 	cmd := c.Client.Get(key)
 	val, err := cmd.Result()
 	if err != nil {
@@ -46,7 +47,7 @@ func (c *RDClient) Del(key string) bool {
 	return true
 }
 
-func (c *RDClient) Exists(key string) (bool) {
+func (c *RDClient) Exists(key string) bool {
 	v, err := c.Client.Exists(key).Result()
 	if err != nil {
 		klog.Errorf("exists key %s failed: %v", key, err)
@@ -55,3 +56,58 @@ func (c *RDClient) Exists(key string) (bool) {
 	return v > 0
 }
 
+func (c *RDClient) HGet(key string, field string) string {
+	val, err := c.Client.HGet(key, field).Result()
+	if err != nil {
+		klog.Errorf("Hget key %s field %s failed: %v", key, field, err)
+	}
+	return val
+}
+
+func (c *RDClient) HGetInt(key string, field string) int {
+	val, err := c.Client.HGet(key, field).Result()
+	if err != nil {
+		klog.Errorf("Hget key %s field %s failed: %v", key, field, err)
+	}
+	count, err := strconv.Atoi(val)
+	if err != nil {
+		klog.Errorf("Hget key %s field %s failed: %v", key, field, err)
+	}
+	return count
+}
+
+func (c *RDClient) HSet(key string, field string, value interface{}) bool {
+	err := c.Client.HSet(key, field, value).Err()
+	if err != nil {
+		klog.Errorf("Hset key %s field %s failed: %v", key, field, err)
+		return false
+	}
+	return true
+}
+
+func (c *RDClient) HDel(key string, field string) bool {
+	err := c.Client.HDel(key, field).Err()
+	if err != nil {
+		klog.Errorf("Hdel key %s field %s failed: %v", key, field, err)
+		return false
+	}
+	return true
+}
+
+func (c *RDClient) HExists(key string, field string) bool {
+	v, err := c.Client.HExists(key, field).Result()
+	if err != nil {
+		klog.Errorf("Hexists key %s field %s failed: %v", key, field, err)
+		return false
+	}
+	return v
+}
+
+func (c *RDClient) HIncr(key string, field string, num int64) bool {
+	err := c.Client.HIncrBy("user_1", "count", num).Err()
+	if err != nil {
+		klog.Errorf("Hinc key %s field %s failed: %v", key, field, err)
+		return false
+	}
+	return true
+}
