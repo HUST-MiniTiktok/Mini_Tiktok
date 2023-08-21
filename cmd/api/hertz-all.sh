b4f45a12f6df19f2c93f95b3ac05b6761b94271e
Tianze Yu <yutianzeabc@126.com>
2023-08-21 22:10:26 +0800

    New: hertz-all.sh

---
+++ cmd/api/hertz-all.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+hz update -idl ../../idl/user.thrift
+hz update -idl ../../idl/publish.thrift
+hz update -idl ../../idl/feed.thrift
+hz update -idl ../../idl/favorite.thrift
+hz update -idl ../../idl/comment.thrift
+hz update -idl ../../idl/relation.thrift
+hz update -idl ../../idl/message.thrift
